88dc0209d166448ead03cb5f9d4f422a024af9fc
nidhikamath <kamathnidhi98@gmail.com>
2020-10-18 12:08:29 +0530

    stateful and stateless widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,68 @@
 import 'package:flutter/material.dart';
 void main() {
   runApp(
-    new MyApp()
+    new MyApp1()
   );
 }
 
-class MyApp extends StatefulWidget{
+
+class MyApp extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-    new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Basic Concepts of Flutter"),
-        elevation: 5.0,
-      ),
-      body: new Center(
-        child: Text(
-          'Hello, world!',
-          textDirection: TextDirection.ltr,
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String text = "";
+
+  @override
+  void initState() {
+    text = "Click here to change text";
+    super.initState();
+  }
+
+  void changeText(){
+    setState(() {
+      text = "Hello Flutter";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: new Scaffold(
+        body: new Center(
+          child: new RaisedButton(onPressed: (){
+            changeText();
+          },
+          child: new Text(text),),
+
         ),
       ),
     );
   }
-
 }
+
+
+class MyApp1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "Flutter Basic Concepts",
+      home: new Scaffold(
+        body: statelessWidget()
+      ),
+    );
+  }
+
+  statelessWidget() {
+    return new Container(
+      color:  Colors.blueAccent,
+      margin: EdgeInsets.all(10.0),
+      child: new Container(
+        color: Colors.black26,
+        margin: EdgeInsets.all(10.0),
+      ),
+    );
+  }
+}
+
